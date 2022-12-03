4f892ca936cafd3bb6dfcb535106214de93c1f82
SeolYoungKim <nasur4da@gmail.com>
2022-12-03 22:32:09 +0900

    refactor(LottoNumberGenerator): unmodifiableList를 반환하지 않고, copyOf를 통해 복사된 값을 반환하도록 수정.

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,9 +1,10 @@
 package lotto.domain;
 
-import static camp.nextstep.edu.missionutils.Randoms.*;
-import static lotto.domain.Lotto.*;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.domain.Lotto.MAX_NUMBER_OF_LOTTO;
+import static lotto.domain.Lotto.MIN_NUMBER_OF_LOTTO;
+import static lotto.domain.Lotto.SIZE_OF_LOTTO_NUMBERS;
 
-import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
@@ -12,6 +13,6 @@ public class LottoNumberGenerator {
         List<Integer> randomNumbers = pickUniqueNumbersInRange(MIN_NUMBER_OF_LOTTO,
                 MAX_NUMBER_OF_LOTTO, SIZE_OF_LOTTO_NUMBERS);
 
-        return Collections.unmodifiableList(randomNumbers);
+        return List.copyOf(randomNumbers);
     }
 }
