db5e71546dcdcbd5c986ac425f8970b345b79291
SeolYoungKim <nasur4da@gmail.com>
2022-12-03 21:40:57 +0900

    feat(BonusNumber): 보너스 숫자 검증 로직 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -1,10 +1,20 @@
 package lotto.domain;
 
+import static lotto.domain.Lotto.MAX_NUMBER_OF_LOTTO;
+import static lotto.domain.Lotto.MIN_NUMBER_OF_LOTTO;
+
 public class BonusNumber {
 
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
+        validateRangeOfNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateRangeOfNumber(int bonusNumber) {
+        if (bonusNumber < MIN_NUMBER_OF_LOTTO || MAX_NUMBER_OF_LOTTO < bonusNumber) {
+            throw new IllegalArgumentException("숫자의 범위는 1~45여야 합니다.");
+        }
     }
 }
